548d4709e066b8169a3e0555e0ad7d2fba658cbe
yusuf <yusuffront@gmail.com>
2022-05-26 13:55:41 +0800

    updated several codes --tugas updated

--- pages/tugas/index.php
+++ pages/tugas/index.php
@@ -1,1 +1,33 @@
-<h1>Tugas</h1>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">MataKuliah</h1>
+    </div>
+
+    <div class="table-responsive">
+        <?php 
+            if($_SESSION['message'] != "kosong"){
+        ?>
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong><?php echo $_SESSION['message']?></strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        <?php
+        $_SESSION['message'] = "kosong";
+            }
+        ?>
+        <a href="?page=tugas_create" class="btn btn-success btn-sm mb-3"><span data-feather="plus"></span> Buat Tugas</a>
+
+        <table id="example" class="table table-striped" style="width:100%">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Dosen Pengampu</th>
+                <th>MataKuliah</th>
+                <th>Hari</th>
+                <th>Jam</th>
+                <th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+        </table>
+</div>
---
+++ pages/tugas/tugas_create.php
@@ -0,0 +1,87 @@
+<!-- ACTION ADD DATA -->
+<?php 
+    if(isset($_POST['btn-simpan'])){
+        
+        $nama_tugas = $_POST['nama_tugas'];
+        $matakuliah_id = $_POST['matakuliah_id'];
+        $keterangan = $_POST['keterangan'];
+        $deadline_date = $_POST['deadline_date'];
+        $deadline_time = $_POST['deadline_time'];
+
+        $createSQL = "INSERT INTO `tugas` (`id`,`nama_tugas`, `matakuliah_id`, `keterangan`, `deadline_date`, `deadline_time`) VALUES (NULL, ?,?,?,?,?)";
+
+        include_once("../database/database.php");
+        $database = new Database;
+        $connection = $database->getConnection();
+        $statement = $connection->prepare($createSQL);
+        $statement->bindParam(1, $nama_tugas);
+        $statement->bindParam(2, $matakuliah_id);
+        $statement->bindParam(3, $keterangan);
+        $statement->bindParam(4, $deadline_date);
+        $statement->bindParam(5, $deadline_time);
+        $statement->execute();
+?>
+
+<?php
+    $_SESSION['message'] = "Tugas Baru Berhasil Dibuat";
+    header('Location: main.php?page=tugas');
+    }
+?>
+
+<?php 
+    include_once("../database/database.php");
+    $database = new Database;
+    $connection = $database->getConnection();
+
+    $sql = "SELECT id, nama_matakuliah FROM matakuliah";
+    $statement = $connection->prepare($sql);
+    $statement->execute();
+    $statement->setFetchMode(PDO::FETCH_ASSOC);
+    
+?>
+
+<!-- ACTION ADD DATA -->
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Tambah Tugas Baru</h1>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+            <form action="" method="post">
+                <div class="mb-3">
+                    <label for="nama_tugas" class="form-label">Nama Tugas</label>
+                    <input type="text" name="nama_tugas" id="" class="form-control">
+                </div>
+                <div class="mb-3">
+                    <label for="matakuliah_id" class="form-label">MataKuliah</label>
+                    <select name="matakuliah_id" class="form-select">
+                        <?php 
+                            while($data = $statement->fetch()){
+                                echo '<option value='.$data['id'].'>'.$data['nama_matakuliah'].'</option>';
+                            }
+                        ?>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="hari" class="form-label">Keterangan</label>
+                    <textarea name="keterangan" id="" cols="30" rows="4" class="form-control" placeholder="Masukkan Keterangan"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="deadline" class="form-label">Deadline</label>
+                    <div class="row mb-3">
+                        <div class="col-sm-6">
+                            <input type="date" name="deadline_date" class="form-control">
+                        </div>
+                        <div class="col-sm-6">
+                            <input type="time" name="deadline_time" class="form-control">
+                        </div>
+                    </div>
+                </div>
+    
+                <div class="d-grid gap-2 mb-3">
+                    <button class="btn btn-primary" name="btn-simpan" type="submit">Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
