00fb8dd5b0f8cf4fad91cd3cf116231333e2068d
yusuf <yusuffront@gmail.com>
2022-05-29 18:12:34 +0800

    updated several codes --dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,1 +1,31 @@
-<h1>Dashboard</h1>
+<div class="container">
+    <div class="p-4 text-center">
+        <h1>Dashboard</h1>
+    </div>
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <h3 class="text-center text-primary"> Dosen</h3>
+                <div class="m-2">
+                    <a href="?page=dosen" class="btn btn-primary"><span data-feather="user"></span> Dosen View..</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <h3 class="text-center text-primary"> Matakuliah</h3>
+                <div class="m-2">
+                    <a href="?page=matakuliah" class="btn btn-primary"><span data-feather="user"></span> Matakuliah View..</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+                <h3 class="text-center text-primary"> Tugas</h3>
+                <div class="m-2">
+                    <a href="?page=tugas" class="btn btn-primary"><span data-feather="user"></span> Tugas View..</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
